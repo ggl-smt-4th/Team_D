ab46ad725bfb9eab0889364c504e22a57c686afb
王戬深 <jianshen.wang@corp.elong.com>
2018-06-24 01:43:41 +0800

    提交第二课作业

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -2,57 +2,107 @@
 pragma solidity ^0.4.14;
 
 contract PayRoll{
-    uint salary;
-    address mainAddr;
-    
-    address canPaidAddr;
-    uint payDurtion = 10 seconds;
-    uint lastPayDay = now;
-    function PayRoll(){
-        mainAddr = msg.sender;
+    struct Employee{
+        address id;
+        uint salary;
+        uint lastPayDay;
     }
-    function updateEmployee(address userAddr,uint money) returns (bool){
-        require(msg.sender==mainAddr);
+    
+    address owner;
+    uint payDurtion = 10 seconds;
+    Employee[] employees;
+    
+    uint totaSalary;
+    
+    function PayRoll(){
         
-        if(userAddr != 0x0){
-            uint payment = salary* ((now - lastPayDay)/payDurtion);
-            canPaidAddr.transfer(payment);
-        }
+        owner = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
         
-        if(canPaidAddr!=userAddr){
-            canPaidAddr=userAddr;
+    }
+    
+    function _partialPaid(Employee employee) private{
+        uint payment = employee.salary * (now - employee.lastPayDay)/payDurtion;
+        employee.id.transfer(payment);   
+    }
+    function _findEmployee(address employeeId) private returns (Employee,uint){
+         for(uint i=0;i<employees.length;i++){
+              if(employees[i].id==employeeId){
+                  return (employees[i],i);
+              }
+         }
+    }
+    function addEmployee(address employeeId ,uint salary){
+        require(msg.sender==owner);
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id == 0x0);
+        uint userSalary = salary * 1 ether;
+        employees.push(Employee(employeeId,userSalary,now));
+        totaSalary+=userSalary;
+        
+    }
+    function removeEmployee(address employeeId){
+        require(msg.sender==owner);
+        
+        assert(employees.length>0);
+        
+        var (employee, index) = _findEmployee(employeeId);
+        
+        assert(employee.id != 0x0);
+        
+        _partialPaid(employee);
+        delete employees[index];
+        if(employees.length>1){
+            employees[index]=employees[employees.length-1];    
         }
-        if(salary!=money){
-            salary = money * 1 ether;
-        }
-        lastPayDay=now;
-        return true;
+    
+        employees.length-=1;
+        totaSalary-=employee.salary;
+    }
+    function updateEmployee(address employeeId,uint salary) {
+        require(msg.sender==owner && employees.length>0);
+        
+        // assert(employees.length>0);
+        
+        var (employee, index) = _findEmployee(employeeId);
+    
+        assert(employee.id!= 0x0 && employee.salary != salary);
+        
+        _partialPaid(employee);
+        totaSalary -= (employee.salary-salary);
+        employees[index].salary = salary;
+        employees[index].lastPayDay=now;
     }
     
     function addFund() payable returns (uint){
-        require(msg.sender==mainAddr);
+        
         return this.balance;
     }
     
     function calculteRunway() returns (uint){
-        require(msg.sender==mainAddr);
-        return this.balance / salary;
+        require(msg.sender==owner);
+        //uint totaSalary;
+        // for(uint i=0;i<employees.length;i++){
+        //     totaSalary += employees[i].salary;
+        // }
+        
+        return  this.balance / totaSalary;
     }
     
     function hasEnoughFund() returns (bool){
-        require(msg.sender==mainAddr);
+        require(msg.sender==owner);
         return calculteRunway()>0;
     }
     
     function getPaid(){
-        require(msg.sender==canPaidAddr);
+        var (employee, index) = _findEmployee(msg.sender);
+        assert(employee.id != 0x0);
         
-        uint nextPayDay = lastPayDay + payDurtion;
+        uint nextPayDay = employee.lastPayDay + payDurtion;
         
         assert(nextPayDay < now);
         
-        lastPayDay = nextPayDay;
-        canPaidAddr.transfer(salary);
+        employee.lastPayDay = nextPayDay;
+        employee.id.transfer(employee.salary);
         
     }
 
