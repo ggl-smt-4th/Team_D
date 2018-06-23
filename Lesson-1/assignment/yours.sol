65d3e3241357cece8e411034944bf8ed3f42e8f3
王戬深 <jianshen.wang@corp.elong.com>
2018-06-23 22:45:09 +0800

    删掉多余方法

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.14;
 
 contract PayRoll{
     uint salary;
-    //main account
     address mainAddr;
     
     address canPaidAddr;
@@ -17,11 +16,9 @@ contract PayRoll{
         
         if(userAddr != 0x0){
             uint payment = salary* ((now - lastPayDay)/payDurtion);
-            
             canPaidAddr.transfer(payment);
         }
         
-        //bool needRevert = false;
         if(canPaidAddr!=userAddr){
             canPaidAddr=userAddr;
         }
@@ -31,32 +28,7 @@ contract PayRoll{
         lastPayDay=now;
         return true;
     }
-    function changeAddr(address userAddr) returns (bool){
-            require(msg.sender==mainAddr);
-            
-
-            if(canPaidAddr==userAddr){
-                 revert();
-            }
-            
-            canPaidAddr=userAddr;
-            
-            return true;
-    }    
-    function changeMoney(address userAddr,uint money) returns (bool){
-            require(msg.sender==mainAddr);
-            // bool needRevert = false;
-            if(userAddr!=canPaidAddr){
-                revert();
-            }
-            if(salary==money){
-                revert();
-            }
-            
-            salary = money;
-            
-            return true;
-    }   
+    
     function addFund() payable returns (uint){
         require(msg.sender==mainAddr);
         return this.balance;
@@ -76,15 +48,12 @@ contract PayRoll{
         require(msg.sender==canPaidAddr);
         
         uint nextPayDay = lastPayDay + payDurtion;
+        
         assert(nextPayDay < now);
+        
         lastPayDay = nextPayDay;
         canPaidAddr.transfer(salary);
         
     }
-    
-    function querySelfMoney() payable returns (uint){
-        
-        require(msg.sender==canPaidAddr);
-        return canPaidAddr.balance;
-    }
+
 }
