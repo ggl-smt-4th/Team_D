e8a7c28868a559639c849c26c64b47708c9a198b
mitDarkMomo <18507487716@163.com>
2018-06-26 16:31:42 +0800

    作业完成

--- projects/lesson-3/contracts/Payroll.sol
+++ projects/lesson-3/contracts/Payroll.sol
@@ -8,30 +8,64 @@ contract Payroll is Ownable {
     using SafeMath for uint;
 
     struct Employee {
-        // TODO, your code here
+        address id;
+        uint salary;
+        uint lastPayday;
     }
 
     uint constant payDuration = 30 days;
-    uint public totalSalary = 0;
+    uint public totalSalary;
+    
+    mapping(address => Employee) public employees;
+    
+    
+    modifier employeeExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _;
+    }
+    
+    
+    function _payOff(Employee employee) private {
+        uint payment = employee.salary.mul(now.sub(employee.lastPayday)).div(payDuration);
+        employee.id.transfer(payment);
+    }
+    
 
-    function Payroll() payable public {
-        // TODO: your code here
+    function addEmployee(address employeeId, uint salary) public onlyOwner{
+        var empl = employees[employeeId];
+        assert(empl.id == 0x0);
+        
+        uint sal = sal.mul(1 ether);
+        totalSalary = totalSalary.add(sal);
+        employees[employeeId] = Employee(employeeId, sal, now);
+    }
+    
+
+    function removeEmployee(address employeeId) public onlyOwner employeeExist(employeeId) {
+        var empl = employees[employeeId];
+        
+        _payOff(empl);
+        totalSalary = totalSalary.sub(empl.salary);
+        delete employees[employeeId];
     }
 
-    function addEmployee(address employeeId, uint salary) public {
-        // TODO: your code here
+    function changePaymentAddress(address oldAddress, address newAddress) public onlyOwner employeeExist(oldAddress) {
+        var empl = employees[oldAddress];
+        
+        employees[newAddress] = Employee(newAddress, empl.salary, empl.lastPayday);
+        delete employees[oldAddress];
     }
 
-    function removeEmployee(address employeeId) public {
-        // TODO: your code here
-    }
-
-    function changePaymentAddress(address oldAddress, address newAddress) public {
-        // TODO: your code here
-    }
-
-    function updateEmployee(address employeeId, uint salary) public {
-        // TODO: your code here
+    function updateEmployee(address employeeId, uint salary) public onlyOwner employeeExist(employeeId) {
+        var empl = employees[employeeId];
+        
+        _payOff(empl);
+        
+        uint sal = salary.mul(1 ether);
+        totalSalary = totalSalary.sub(empl.salary).add(sal);
+        empl.salary = sal;
+        empl.lastPayday = now;
     }
 
     function addFund() payable public returns (uint) {
@@ -39,14 +73,20 @@ contract Payroll is Ownable {
     }
 
     function calculateRunway() public view returns (uint) {
-        // TODO: your code here
+        return address(this).balance.div(totalSalary);
     }
 
     function hasEnoughFund() public view returns (bool) {
-        // TODO: your code here
+        return calculateRunway() > 0;
     }
 
-    function getPaid() public {
-        // TODO: your code here
+    function getPaid() public employeeExist(msg.sender) {
+        var empl = employees[msg.sender];
+        
+        uint nextPayday = empl.lastPayday.sub(payDuration);
+        assert(nextPayday < now);
+        
+        empl.lastPayday = nextPayday;
+        empl.id.transfer(empl.salary);
     }
 }
