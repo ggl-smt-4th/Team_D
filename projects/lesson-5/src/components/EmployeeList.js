1f78eeaa52abdf2cb04b9cb01ef00bc5dfdc59bc
ninjaahhh <junjiah@protonmail.com>
2018-07-10 12:40:45 -0700

    40_和均价 第五课作业

--- projects/lesson-5/src/components/EmployeeList.js
+++ projects/lesson-5/src/components/EmployeeList.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
-import { Table, Button, Modal, Form, InputNumber, Input, message, Popconfirm } from 'antd';
+import React, { Component } from "react";
+import {
+  Table,
+  Button,
+  Modal,
+  Form,
+  InputNumber,
+  Input,
+  message,
+  Popconfirm
+} from "antd";
 
-import EditableCell from './EditableCell';
+import EditableCell from "./EditableCell";
 
 const FormItem = Form.Item;
 
-const columns = [{
-  title: '地址',
-  dataIndex: 'address',
-  key: 'address',
-}, {
-  title: '薪水',
-  dataIndex: 'salary',
-  key: 'salary',
-}, {
-  title: '上次支付',
-  dataIndex: 'lastPaidDay',
-  key: 'lastPaidDay',
-}, {
-  title: '操作',
-  dataIndex: '',
-  key: 'action'
-}];
+const columns = [
+  {
+    title: "地址",
+    dataIndex: "address",
+    key: "address"
+  },
+  {
+    title: "薪水",
+    dataIndex: "salary",
+    key: "salary"
+  },
+  {
+    title: "上次支付",
+    dataIndex: "lastPaidDay",
+    key: "lastPaidDay"
+  },
+  {
+    title: "操作",
+    dataIndex: "",
+    key: "action"
+  }
+];
 
 class EmployeeList extends Component {
   constructor(props) {
@@ -36,71 +50,111 @@ class EmployeeList extends Component {
     columns[1].render = (text, record) => (
       <EditableCell
         value={text}
-        onChange={ this.updateEmployee.bind(this, record.address) }
+        onChange={this.updateEmployee.bind(this, record.address)}
       />
     );
 
     columns[3].render = (text, record) => (
-      <Popconfirm title="你确定删除吗?" onConfirm={() => this.removeEmployee(record.address)}>
+      <Popconfirm
+        title="你确定删除吗?"
+        onConfirm={() => this.removeEmployee(record.address)}
+      >
         <a href="#">Delete</a>
       </Popconfirm>
     );
   }
 
   componentDidMount() {
+    const { payroll, account } = this.props;
+    payroll.getEmployerInfo
+      .call({
+        from: account
+      })
+      .then(result => {
+        const employeeCount = result[2].toNumber();
+
+        if (employeeCount === 0) {
+          this.setState({ loading: false });
+          return;
+        }
+
+        this.loadEmployees(employeeCount);
+      });
+  }
+
+  async loadEmployees(employeeCount) {
     const { payroll, account, web3 } = this.props;
-    payroll.checkInfo.call({
-      from: account
-    }).then((result) => {
-      const employeeCount = result[2].toNumber();
-
-      if (employeeCount === 0) {
-        this.setState({loading: false});
-        return;
-      }
-
-      this.loadEmployees(employeeCount);
+    const infoPromises = [...Array(employeeCount).keys()].map(i =>
+      payroll.getEmployeeInfo(i, { from: account })
+    );
+    const infoResults = await Promise.all(infoPromises);
+    this.setState({
+      employees: infoResults.map(info => ({
+        key: info[0],
+        address: info[0],
+        salary: web3.fromWei(info[1].toNumber()),
+        lastPaidDay: new Date(info[2].toNumber() * 1000).toISOString(),
+      })),
+      loading: false
     });
-
   }
 
-  loadEmployees(employeeCount) {
+  async addEmployee() {
+    const { address, salary: salaryInEther } = this.state;
+    const { payroll, account } = this.props;
+    this.setState({ showModal: false });
+    try {
+      await payroll.addEmployee(address, salaryInEther, { from: account });
+    } catch (error) {
+      console.log(error);
+      alert("Failed to add employee: ", error);
+    }
   }
 
-  addEmployee = () => {
-  }
+  async updateEmployee(address, salary) {
+    const { payroll, account } = this.props;
+    try {
+        await payroll.updateEmployee(address, salary, { from: account });
+    } catch (error) {
+      console.log(error);
+      alert("Failed to update employee: ", error);
+    }
+  };
 
-  updateEmployee = (address, salary) => {
-  }
-
-  removeEmployee = (employeeId) => {
-  }
+  async removeEmployee(employeeId) {
+    const { payroll, account } = this.props;
+    try {
+        await payroll.removeEmployee(employeeId, { from: account });
+    } catch (error) {
+      console.log(error);
+      alert("Failed to remove employee: ", error);
+    }
+  };
 
   renderModal() {
-      return (
+    return (
       <Modal
-          title="增加员工"
-          visible={this.state.showModal}
-          onOk={this.addEmployee}
-          onCancel={() => this.setState({showModal: false})}
+        title="增加员工"
+        visible={this.state.showModal}
+        onOk={this.addEmployee.bind(this)}
+        onCancel={() => this.setState({ showModal: false })}
       >
         <Form>
           <FormItem label="地址">
             <Input
-              onChange={ev => this.setState({address: ev.target.value})}
+              onChange={ev => this.setState({ address: ev.target.value })}
             />
           </FormItem>
 
           <FormItem label="薪水">
             <InputNumber
               min={1}
-              onChange={salary => this.setState({salary})}
+              onChange={salary => this.setState({ salary })}
             />
           </FormItem>
         </Form>
       </Modal>
     );
-
   }
 
   render() {
@@ -109,21 +163,17 @@ class EmployeeList extends Component {
       <div>
         <Button
           type="primary"
-          onClick={() => this.setState({showModal: true})}
+          onClick={() => this.setState({ showModal: true })}
         >
           增加员工
         </Button>
 
         {this.renderModal()}
 
-        <Table
-          loading={loading}
-          dataSource={employees}
-          columns={columns}
-        />
+        <Table loading={loading} dataSource={employees} columns={columns} />
       </div>
     );
   }
 }
 
-export default EmployeeList
+export default EmployeeList;
--- projects/lesson-5/truffle.js
+++ projects/lesson-5/truffle.js
@@ -1,10 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infuraAPI = process.env.INFURA_API;
+var mnemonic = process.env.MNEMONIC;
+
 module.exports = {
-  migrations_directory: "./migrations",
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infuraAPI),
+      network_id: 3,
+      gas: 2500000,
+      gasPrice: 100000000000,
     }
   }
 };
